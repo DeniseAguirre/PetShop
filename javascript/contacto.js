1477cebd155339dd6e3b7b261934fa736fee99f3
franciscopedicino <franciscopedi91@gmail.com>
2023-04-18 15:53:59 -0300

    francisco 18

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -1,24 +1,19 @@
 const boton=document.getElementById(`boton`)
 const form = document.getElementById(`formulario`)
 
-
-console.log(nombre.value)
 boton.addEventListener( `click`,()=>{
     const nombre  = document.getElementById(`nombre`)
 const apellido = document.getElementById(`apellido`)
 const comentario= document.getElementById(`comentario`)
+const email=document.getElementById(`email`)
 
-if (!nombre.value||!apellido.value||!comentario.value){
+
+if (!nombre.value||!apellido.value||!comentario.value||!email.value){
     swal ( "Ups" ,  "Tiene que rellenar los campos!" ,  "error" )
     
 } else{
-
     swal("Que bueno!", "La Consulta se ha enviado correctamente!", "success");
-
-
 }
+    
 })
-
-
-
 
