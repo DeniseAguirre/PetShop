9b08de0846ad6f156847848fc310e7c2bbaea07c
jdosing <96783149+jdosing@users.noreply.github.com>
2023-04-18 17:25:54 -0500

    nueva version

--- javascript/detalles.js
+++ javascript/detalles.js
@@ -22,16 +22,18 @@ console.log(idArticulos)
 function crearDetalles( detalle ){
     let template=``
     template = `
-    <div class="row p-1 d-flex justify-content-center" style="background-color: rgb(242, 225, 155);">
-    <div class="col-10 col-md-6" >
-            <img class="img-fluid " id="imgarticles" src="${detalle.imagen}" alt="..." >
+    <div class="row contenedorgeneral p-1 d-flex justify-content-center" style="background-color: rgb(242, 225, 155);">
+    <div class=" text-center col-md-6" >
+            <img class="img-fluid col-6 pt-4 " id="imgarticles" src="${detalle[1].imagen}" alt="..." >
         </div>
-                    <div class="p-4 col-7 d-flex text-center" id="nuevo">
-                        <h4 class="card-title">NOMBRE PRODUCTO</h4>
-                        <p>${detalle.nombre}</p>   
-                        <p><b>Categoria: ${detalle.categoria}}</p> 
-                        <p><b>DISPONIBLES: ${detalle.disponibles}}</p> 
-                            <p><b>Price: ${detalle.precio}}</p> 
+                    <div class="p-1 col-6 d-flex text-center"id="nuevo">
+                        <h5 class="card-title">NOMBRE PRODUCTO</h5>
+                        <h3><b>${detalle[1].producto}</b></h3>   
+                        <p><Categoria: ${detalle[0].categoria}</p> 
+                        <p><b><b>Descripcion:</b></p>
+                        <p>${detalle[1].descripcion}</p>
+                        <p><b>DISPONIBLES: ${detalle[1].disponibles}</p> 
+                        <h3>Price: $${detalle[1].precio}</h3> 
         </div>
 
     
